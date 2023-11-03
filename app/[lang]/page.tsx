eee9b5c246ea229ac0295b1c1246a12c84110e7b
Abhinand Arun <abhinandarun2015@gmail.com>
2023-11-04 00:11:07 +0530

    feat: Add logo and seo

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,13 +4,19 @@ import { Dictionary } from '@/types/dictionary'
 import { Locale } from '@/i18n-config'
 import Link from 'next/link'
 import { ChevronRight } from 'lucide-react'
+import Image from 'next/image'
+import Logo from '../../public/logo.png'
 
 export default async function RecordPage({ params }: { params: { lang: Locale } }) {
   const dict: Dictionary = await getDictionary(params.lang)
 
   return (
     <main className="p-4">
-      <div className="flex h-12 w-full items-center justify-end pr-12">
+      <div className="flex h-12 w-full items-center justify-between px-8">
+        <Link href="/" className="flex gap-4">
+          <Image src={Logo} height={600} width={600} className="h-[60px] w-[60px]" alt="Logo" />
+          <div>Healthify</div>
+        </Link>
         <div className="flex cursor-pointer font-medium text-blue-600 hover:underline dark:text-blue-500">
           {params.lang == 'en' && <Link href="/hi">हिंदी</Link>}
           {params.lang == 'hi' && <Link href="/en">English</Link>}
